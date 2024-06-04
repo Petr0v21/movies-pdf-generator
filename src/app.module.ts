134bfe431743058563a2facd3cb9d659937fb094
Petr0v21 <petr0v21vs@gmail.com>
2024-06-04 15:56:04 +0300

    hotfix

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,8 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { MoviesModule } from './movies/movies.module';
 
 @Module({
   imports: [ConfigModule.forRoot(), MoviesModule],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
